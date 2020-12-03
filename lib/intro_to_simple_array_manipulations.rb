b6a9f139c8f8d27439bf35dc1cf11df2b9f2b8d8
Erik Huang <erikhuangdrums@gmail.com>
2020-12-03 19:29:50 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(arr, str)
+  arr.push(str)
+  return arr
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+  return arr
+end
+
+def using_pop(arr)
+  return arr.pop
+end
+
+def pop_with_args(arr)
+  fuck_off = arr.pop(2)
+  return fuck_off
+end
+
+def using_shift(arr)
+  return arr.shift
+end
+
+def shift_with_args(arr)
+  fuck_off = arr.shift(2)
+  return fuck_off
+end
